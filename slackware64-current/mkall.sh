da643431bb02e97d9cd5c4dd5052cca342083956
jaloo <liu.jaloo@gmail.com>
2019-10-23 11:54:15 +0800

    cdrom >> drive

--- slackware64-current/mkall.sh
+++ slackware64-current/mkall.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # URL: https://qemu.weilnetz.de/doc/qemu-doc.html
 qemu-img create slackware64-current.img 50G
+
 # for install with ISO
-qemu-system-x86_64 -m 2048 -hda slackware64-current.img -cdrom /home/jaloo/src/slackware64-current.iso -boot d
+# qemu-system-x86_64 -m 2048 -hda slackware64-current.img -cdrom /home/jaloo/src/slackware64-current.iso -boot once=d
+qemu-system-x86_64 -m 2048 -drive file=slackware64-current.img,index=0,media=disk,format=raw -drive file=/home/jaloo/src/slackware64-current.iso,index=1,media=cdrom -boot once=d
+
 # run slackware
 # qemu-system-x86_64 -m 2048 -hda slackware64-current.img 
 qemu-system-x86_64 -m 2048 -drive file=slackware64-current.img,index=0,media=disk,format=raw
