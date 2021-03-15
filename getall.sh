1f7b14bba4017fd5384e721db2d7590c1c80c4af
刘家禄 <liu.jialu@eco-ev.com>
2021-03-15 10:54:42 +0800

    update

--- getall.sh
+++ getall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-git clone git://git.qemu.org/qemu.git
+# URL: https://www.qemu.org/download/#source
+git clone https://gitlab.com/qemu-project/qemu.git
 cd qemu
 git submodule init
 git submodule update --recursive
-
-git pull --recurse-submodules
